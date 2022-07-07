8018ac8c489f99bdb8f5f6a2fd83972881b22f93
achala dias <achalad@wso2.com>
2022-07-07 22:00:32 +0530

    Choreo Toml Paser V2

---
+++ enums.js
@@ -0,0 +1,12 @@
+const ChoreoRepository = {
+    CHOREO_MANAGE: "ChoreoManaged",
+    USER_MANAGE: "UserManagedEmpty",
+    USER_MANAGE_NON_EMPTY: "UserManagedNonEmpty",
+}
+
+const ChoreoBasicTemplates = ["service", "main", "webhook"]
+
+module.exports = {
+    ChoreoRepository: ChoreoRepository,
+    ChoreoBasicTemplates: ChoreoBasicTemplates
+}
